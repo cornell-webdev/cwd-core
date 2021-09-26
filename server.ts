a08b2f2c47b06bdd91c6d6fd8d4097da3c7d3655
Jay Joo <jj534@cornell.edu>
2021-09-26 02:42:43 -0400

    Serve index regardless of path

--- server.ts
+++ server.ts
@@ -2,9 +2,12 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 app.use(express.static(path.join(__dirname, 'build')))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'))
+})
 
 // force https
 app.enable('trust proxy')
