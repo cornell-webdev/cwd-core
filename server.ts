7013471bb32fd5992800b9be8ae3a9703a1e2153
Jay Joo <jj534@cornell.edu>
2021-09-26 01:57:10 -0400

    Force https

--- server.ts
+++ server.ts
@@ -4,6 +4,12 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 
+// force https
+app.enable('trust proxy')
+app.use((req, res, next) => {
+  req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
+})
+
 app.use(express.static(path.join(__dirname, 'build')))
 
 app.listen(port, () => console.log(`App is live on port ${port}!`))
