93ea770813c244f4295ed82cbe1c23aeb11ed801
Jay Joo <jj534@cornell.edu>
2021-09-27 11:55:15 -0400

    Fix force https bug (attempt)

--- server.ts
+++ server.ts
@@ -4,21 +4,15 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3001
 
-app.use(express.static(path.join(__dirname, 'build')))
-
 // force https
-app.enable('trust proxy')
 app.use((req, res, next) => {
-  if (
-    !req.secure &&
-    req.get('x-forwarded-proto') !== 'https' &&
-    process.env.NODE_ENV === 'production'
-  ) {
+  if (!req.secure && process.env.NODE_ENV === 'production') {
     return res.redirect(`https://${req.get('host')}${req.url}`)
   }
   next()
 })
 
+app.use(express.static(path.join(__dirname, 'build')))
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build/index.html'))
 })
