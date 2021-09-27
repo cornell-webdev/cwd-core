08a7d4d062af81c5347eaaf6063444d994bf520c
Jay Joo <jj534@cornell.edu>
2021-09-27 11:22:31 -0400

    Force https before serving static file

--- server.ts
+++ server.ts
@@ -5,14 +5,15 @@ const app = express()
 const port = process.env.PORT || 3001
 
 app.use(express.static(path.join(__dirname, 'build')))
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build/index.html'))
-})
 
 // force https
 app.enable('trust proxy')
 app.use((req, res, next) => {
   req.secure ? next() : res.redirect('https://' + req.headers.host + req.url)
+})
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build/index.html'))
 })
 
 app.listen(port, () => console.log(`App is live on port ${port}!`))
