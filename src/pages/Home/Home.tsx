e2e41cf9b4ccdc93f9d020fa655d1b826f6f3eb6
Jay Joo <jj534@cornell.edu>
2021-09-30 09:24:41 -0400

    Update project links

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -80,6 +80,7 @@ const Home = () => {
         p1='Glue is a centralized design system and React component library for Cornell WebDev.'
         p2='Building a comprehensive design system is hard work! But your work on Glue will impact
         all the projects within Cornell WebDev. Talk about lots of responsibility!'
+        link='https://github.com/cornell-webdev/glue-ui'
         svg={<Illust3 />}
       />
       <Showcase
@@ -87,7 +88,7 @@ const Home = () => {
         name='Core'
         p1="Core will be working on this very website. Yep! The one you're looking at right now!"
         p2='This subteam will be responsible for the face of Cornell WebDev. First impressions matter. Remember, we only have 10 seconds!'
-        link='https://github.com/jj534/cornell-webdev-home'
+        link='https://github.com/cornell-webdev/cwd-core'
         svg={<IllustCore />}
       />
       <Showcase
